dbcc27fabd45ac3b119263563885054481f7367b
Outvi V <git@outv.im>
2021-02-14 22:58:08 +0800

    chore: move configurations to secrets.ts

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -1,9 +1,5 @@
 export {}
 
 declare global {
-  const BOT_TOKEN: string
-  const DOMAIN: string
-  const WHITELISTED_HOSTNAMES: string
-  const META_URL: string
   const KV: KVNamespace
 }
---
+++ src/secrets.sample.ts
@@ -0,0 +1,7 @@
+export default {
+  domain: 'hosymbot-worker.endpoint.workers.dev',
+  webhook: 'prod',
+  botToken: 'botid:botkey',
+  whitelistedHostnames: ['asset.storage.example.com'],
+  metaUrl: 'https://example.com/meta/json/path/meta.json',
+}
