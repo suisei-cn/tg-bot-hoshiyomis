d1a49f8b312d916d93a0f21bf8de9f7eb70d2d1c
Outvi V <git@outv.im>
2021-02-15 00:43:51 +0800

    chore: remove whitelistedHostnames

--- src/secrets.sample.ts
+++ src/secrets.sample.ts
@@ -2,6 +2,5 @@ export default {
   domain: 'hosymbot-worker.endpoint.workers.dev',
   webhook: 'prod',
   botToken: 'botid:botkey',
-  whitelistedHostnames: ['asset.storage.example.com'],
   metaUrl: 'https://example.com/meta/json/path/meta.json',
 }
