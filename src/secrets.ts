fcf346276ff889ef8149f7002545608e04a22354
Outvi V <git@outv.im>
2021-02-14 22:59:23 +0800

    chore: remove src/secrets.ts

--- src/secrets.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  domain: DOMAIN,
-  webhook: 'prod',
-  botToken: BOT_TOKEN,
-  whitelistedHostnames: WHITELISTED_HOSTNAMES.split('|').map((x) => x.trim()),
-  metaUrl: META_URL,
-}
