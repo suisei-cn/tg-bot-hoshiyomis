1714b49967898b7687f16cf399b1693840c49ede
Outvi V <git@outv.im>
2021-02-16 14:28:09 +0800

    chore: remove local dev code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import bot from './bot'
 
-function isNode() {
-  return (
-    typeof process === 'object' &&
-    typeof process.versions === 'object' &&
-    typeof process.versions.node !== undefined
-  )
-}
+// function isNode() {
+//   return (
+//     typeof process === 'object' &&
+//     typeof process.versions === 'object' &&
+//     typeof process.versions.node !== undefined
+//   )
+// }
 
-if (isNode()) {
-  // @ts-ignore
-  global.fetch = require('node-fetch')
-  // @ts-ignore
-  global.KV = {
-    get() {
-      return null
-    },
-    put() {},
-  }
-}
+// if (isNode()) {
+//   // @ts-ignore
+//   global.fetch = require('node-fetch')
+//   // @ts-ignore
+//   global.KV = {
+//     get() {
+//       return null
+//     },
+//     put() {},
+//   }
+// }
 
 bot.launch()
