606b68b345fad846a028d0579bd3d402b7aae344
Outvi V <git@outv.im>
2021-02-16 14:15:59 +0800

    chore: dev-related stuffs & patches

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import bot from './bot'
 
+const isNode = new Function(
+  'try {return this===global;}catch(e){return false;}'
+)
+
+if (isNode()) {
+  // @ts-ignore
+  global.fetch = require('node-fetch')
+  // @ts-ignore
+  global.KV = {
+    get() {
+      return null
+    },
+    put() {},
+  }
+}
+
 bot.launch()
