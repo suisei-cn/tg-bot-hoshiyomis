13cc4f48971f4201bd38362ed4911f10472c4f0a
Outvi V <git@outv.im>
2021-08-21 18:02:52 +0800

    feat: /push to update KV

--- src/worker.ts
+++ src/worker.ts
@@ -6,4 +6,19 @@ import bot from './bot'
 
 const router = new Router()
 router.post(`/webhook-${secret.webhook}`, createTelegrafMiddware(bot))
+router.put(`/push-${secret.metadataUpdateEndpoint}`, async ({ req, res }) => {
+  const json = await req.body.json()
+  await KV.put(json.key, json.value)
+  res.type = 'text/plain'
+  res.body = 'OK'
+})
+router.delete(
+  `/push-${secret.metadataUpdateEndpoint}`,
+  async ({ req, res }) => {
+    const json = await req.body.json()
+    await KV.delete(json.key)
+    res.type = 'text/plain'
+    res.body = 'OK'
+  }
+)
 new Application().use(router.middleware).listen()
